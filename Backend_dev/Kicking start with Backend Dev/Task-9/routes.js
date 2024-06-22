9ef8dd479a423a8218b3813774b9de62ea4e37b3
sahil3m3 <sahil3m3@gmail.com>
2024-06-22 14:18:35 +0530

    Task -9 Done

---
+++ Backend_dev/Kicking start with Backend Dev/Task-9/routes.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+
+const routes=(req,res)=>
+{
+
+    if(req.url==='/')
+        {
+
+            let fileText= fs.readFileSync('message.txt', 'utf8');
+
+            res.write(`<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+  <div>
+        ${fileText}
+    </div>
+    <form action="/message" method="POST">
+        <input type="text" name="message">
+        <button type="submit" >Submit</button>
+        
+    </form>
+</body>`)
+return res.end();
+        }
+
+        if (req.url === '/message' && req.method === 'POST') {
+            const body = [];
+            req.on('data', (chunk) => {
+              console.log(chunk);
+              body.push(chunk);
+            });
+            req.on('end', () => {
+              const parsedBody = Buffer.concat(body).toString();
+              const message = parsedBody.split('=')[1];
+              fs.writeFileSync('message.txt', message);
+            });
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+          }
+
+
+res.setHeader('Content-Type',"text/html");
+
+res.write(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My first Code</title>
+</head>
+<body>
+    Hello
+</body>
+</html>`)   
+
+res.end();
+}
+
+
+
+
+module.exports=routes;
