c765aff0e626eb6d6e0551eebd28d3601f5b2465
Sahil Meshram <144378148+Sahil3M3@users.noreply.github.com>
2024-07-29 15:05:52 +0530

    Delete Backend_dev/Expense_Tracker directory

--- Backend_dev/Expense_Tracker/Back-End/routes/password.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const passwordController=require('../controller/passwordController');
-
-
-router.post('/forgotpassword',passwordController.forgotPassword);
-router.post('/resetpassword/:id',passwordController.resetPassword)
-
-module.exports=router;
--- Backend_dev/Expense_Tracker/Back-End/util/database.js
+++
@@ -1,5 +0,0 @@
-const Sequelize=require('sequelize')
-
-const sequelize=new Sequelize('expenses','root','manager',{dialect:'mysql'});
-
-module.exports=sequelize;
