72747b40db1a61a7bf31576b22a64e77c52ef1e5
sahil3m3 <sahil3m3@gmail.com>
2024-07-31 17:09:00 +0530

    Ready to Deploy 1

--- Backend_dev/Expense_Tracker1/Back-End/app.js
+++ Backend_dev/Expense_Tracker1/Back-End/app.js
@@ -2,7 +2,10 @@ const express=require('express');
 const app=express();
 const cors=require('cors');
 require('dotenv').config();
-
+const helmet=require('helmet')
+const morgan=require('morgan')
+const fs=require('fs');
+const path = require('path');
 
 const User=require('./models/user');
 const Expense=require('./models/expense')
@@ -15,12 +18,20 @@ const expenseRouter=require('./routes/expense')
 const sequelize=require('./util/database')
 const purchaseRouter=require('./routes/purchase')
 const passwordRouter=require('./routes/password')
+const asscessLogStream=fs.createWriteStream(path.join(__dirname,'access.log'),
+{flag:'a'} )
+app.use(helmet());
+
+app.use(morgan('combined',{stream:asscessLogStream}));
 app.use(cors());
 app.use(express.json());
 app.use('/user',userRouter)
 app.use('/expense',expenseRouter);
 app.use('/premium',purchaseRouter);
 app.use('/password',passwordRouter);
+app.use('/',(req,res)=>{
+    res.status(404).send("<h1> Not Found</h1>")
+})
 
 
 User.hasMany(Expense, { foreignKey: 'userId', onDelete: 'CASCADE' });
--- Backend_dev/Expense_Tracker1/Back-End/util/database.js
+++ Backend_dev/Expense_Tracker1/Back-End/util/database.js
@@ -1,5 +1,5 @@
 const Sequelize=require('sequelize')
-
-const sequelize=new Sequelize('expenses','root','manager',{dialect:'mysql'});
+require('dotenv').config();
+const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{dialect:'mysql',host:process.DB_HOST});
 
 module.exports=sequelize;
